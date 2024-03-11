4f4892fa073c8a519fda9f9474defd94fb1630d0
syrile omwelema <somwelema@safaricom.co.ke>
2024-03-11 14:13:11 +0300

    terraform file

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  cloud {
+    organization = "innovator"
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+}
